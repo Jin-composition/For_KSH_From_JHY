68eca6178827186f7f9dd821d34df909c3615e6d
SamSung <jinhy921207@naver.com>
2021-03-24 20:39:11 +0900

    Intent 시 Toast Message까지 수정 완료 <MyDiary> 기능만 수정하면 됨

--- app/src/main/java/com/example/for_ksh_from_jhy/MainActivity.java
+++ app/src/main/java/com/example/for_ksh_from_jhy/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
@@ -50,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 Uri uri = Uri.parse("tel:010-9272-2884");
                 Intent intent = new Intent(Intent.ACTION_DIAL, uri);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "언제든지 전화해♥", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -59,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 Uri uri = Uri.parse("http://www.naver.com");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "나의 마지막 목표는 NAVER...!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -67,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "우리 사진좀 찍자!!", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -76,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Gallery_Main.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "눈갱 미안 ><", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -84,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Spinner_Main.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "너와 같이 가고 싶은 곳 들을 모아봤어♥", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -92,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MyDiary.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "나한테 빡치는게 있으면 여기에 적어줘", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "긍정적으로 검토해볼게ㅋㅋㅋ", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -100,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Music_Main.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "어때? 잘 만들었지? ><", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -108,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Fragment_Main.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, "후 방 주 의!!!!!", Toast.LENGTH_SHORT).show();
             }
         });
 
